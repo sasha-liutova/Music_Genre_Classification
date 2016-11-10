1370046cf8fb3dcadf5e6b07a99a231bea39946f
petr.lorenc <petr.lorenc@firma.seznam.cz>
2016-11-10 16:53:27 +0100

    Zakladni SVN (zkouska)

--- genres_classification/main.py
+++ genres_classification/main.py
@@ -1,22 +1,78 @@
 # Beat tracking example
 from __future__ import print_function
+import matplotlib.pyplot as plt
 import librosa
-import numpy
-
-# 1. Get the file path to the included audio example
-filename = librosa.util.example_audio_file()
+from dtw import dtw
+import matplotlib.pyplot as plt
+import numpy as np
+import librosa
+from sklearn import svm
+import IPython.display
+from IPython.display import Image
 
 # 2. Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
-y, sr = librosa.load("rock.00000.au")
+y, sr = librosa.load("./genres/rock/rock.00000.au")
+y2, sr2 = librosa.load("./genres/blues/blues.00000.au")
+y3, sr3 = librosa.load("./genres/classical/classical.00000.au")
+y4, sr4 = librosa.load("./genres/country/country.00000.au")
 
-# 3. Run the default beat tracker
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+mfcc =  librosa.feature.mfcc(y=y, sr=sr)
+mfcc2 =  librosa.feature.mfcc(y=y2, sr=sr2)
+mfcc3 =  librosa.feature.mfcc(y=y3, sr=sr3)
+mfcc4 =  librosa.feature.mfcc(y=y4, sr=sr4)
 
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+mfcc_feat = []
+for item in mfcc:
+    mfcc_feat.extend(item)
 
-# 4. Convert the frame indices of beat events into timestamps
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+mfcc_feat2 = []
+for item in mfcc2:
+    mfcc_feat2.extend(item)
 
-print('Saving output to beat_times.csv')
-librosa.output.times_csv('beat_times.csv', beat_times)
+mfcc_feat3 = []
+for item in mfcc3:
+    mfcc_feat3.extend(item)
+
+mfcc_feat4 = []
+for item in mfcc4:
+    mfcc_feat4.extend(item)
+
+print(mfcc_feat)
+
+# X = [mfcc_feat[:100], mfcc_feat2[:100], mfcc_feat3[:100], mfcc_feat4[:100]]
+X = [mfcc_feat[:100], mfcc_feat2[:100], mfcc_feat3[:100], mfcc_feat4[:100]]
+Y = ["rock", "blues", "classical", "country"]
+
+
+clf = svm.SVC()
+clf.fit(X, Y)
+
+predict_y, predict_sr = librosa.load("./genres/rock/rock.00001.au")
+predict_mfcc =  librosa.feature.mfcc(y=predict_y, sr=predict_sr)
+
+mfcc_predict = []
+for item in predict_mfcc:
+    mfcc_predict.extend(item)
+
+print(clf.predict(mfcc_predict[:100]))
+
+# ---------------------------
+# # tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+# chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+#
+# # print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+#
+# # beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+# print(chroma_stft)
+# print(mfcc)
+#
+# spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+# S, phase = librosa.magphase(librosa.stft(y=y))
+#
+# plt.plot(y)
+# IPython.display.Audio(data=y, rate=sr)
+# librosa.display.specshow(mfcc)
+#
+# # print('Saving output to beat_times.csv')
+# # librosa.output.times_csv('beat_times.csv', beat_times)
