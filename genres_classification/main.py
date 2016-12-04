04eb58342f236ea0f2feeb6d9764ba1c33c182cc
Liutova, Oleksandra <oleksandra.liutova@merck.com>
2016-12-04 16:17:42 +0100

    nic

--- genres_classification/main.py
+++ genres_classification/main.py
@@ -6,11 +6,33 @@ from os import walk
 from os.path import isfile, join
 from sklearn.externals import joblib
 import numpy as np
+import copy
 
 path_to_folder = "./genres_train"
 
 def extract_genre(filenames, dirpath):
     return [filename.split("/")[-2] for filename in [join(dirpath, f) for f in filenames if isfile(join(dirpath, f)) and f.endswith(".au")]]
+
+def preprocess_mfcc(mfcc):
+    mfcc_cp = copy.deepcopy(mfcc)
+    for i in range(mfcc.shape[1]):
+        mfcc_cp[:,i] = mfcc[:,i] - np.mean(mfcc[:,i])
+        mfcc_cp[:,i] = mfcc_cp[:,i]/np.max(np.abs(mfcc_cp[:,i]))
+    return mfcc_cp
+
+def extract_mean_frame(mfcc):
+    features_sum = [0 for i in range(len(mfcc[0]))]
+    for frame in mfcc:
+        index=0
+        for feature in frame:
+            features_sum[index] += feature
+            index += 1
+
+    for i in range(len(mfcc[0])):
+        features_sum[i] = features_sum[i]/len(mfcc)
+
+    return features_sum
+
 
 def predict_genre(name_file, clf):
     predict_y, predict_sr = librosa.load(name_file)
@@ -45,11 +67,17 @@ def train_model():
 
     features_mfcc = []
     for i in range(0,len(array_y)):
-        current_features = librosa.feature.mfcc(y=array_y[i], sr=array_sr[i])
-        features_flat = []
-        for frame in current_features:
-            features_flat.extend(frame)
-        features_mfcc.append(features_flat)
+        current_features = librosa.feature.mfcc(y=array_y[i], sr=array_sr[i], n_mfcc=60)
+        # current_features_preprocessed = preprocess_mfcc(current_features)
+        # features_flat = []
+        # for frame in current_features:
+        #     features_flat.extend(frame)
+        # features_mfcc.append(features_flat)
+        mean_frame = extract_mean_frame(current_features)
+        features_mfcc.append(mean_frame)
+
+    min_length = min([len(x) for x in features_mfcc])
+    features_mfcc_cut = [x[:min_length-1] for x in features_mfcc]
 
     # Extract label for each file
 
@@ -57,26 +85,26 @@ def train_model():
     for (dirpath, dirnames, filenames) in walk(path_to_folder):
         labels.extend(extract_genre(filenames, dirpath))
 
-    X = [x[0:25000] for x in features_mfcc]
-
     # Create a classification model using SVM
 
     clf = svm.SVC()
-    clf.fit(X, labels)
+    clf.fit(features_mfcc_cut, labels)
 
     return clf
 
 
 def main():
-    choose = int(input("Do you want to train new model[1] or use old one[0]"))
-    if choose == 1:
-        clf = train_model()
-        joblib.dump(clf, 'train_model.pkl')
-    else:
-        try:
-            clf = joblib.load('train_model.pkl')
-        except FileNotFoundError:
-            clf = train_model()
+    # choose = int(input("Do you want to train new model[1] or use old one[0]"))
+    # if choose == 1:
+    #     clf = train_model()
+    #     joblib.dump(clf, 'train_model.pkl')
+    # else:
+    #     try:
+    #         clf = joblib.load('train_model.pkl')
+    #     except FileNotFoundError:
+    #         clf = train_model()
+
+    clf = train_model()
 
     print("./genres/blues/blues.00020.au", predict_genre("./genres/blues/blues.00020.au", clf))
     print("./genres/disco/disco.00020.au", predict_genre("./genres/disco/disco.00020.au", clf))
