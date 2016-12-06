01954ac532b15672e22e791a00ed26b0af855282
Liutova, Oleksandra <oleksandra.liutova@merck.com>
2016-12-06 15:11:30 +0100

    visualization

--- genres_classification/main.py
+++ genres_classification/main.py
@@ -10,6 +10,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import copy
+from sklearn.preprocessing import scale
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import pickle
 
 test_path = './genres_train'
 glob_path_train = './genres_split/train'
@@ -136,18 +140,92 @@ def test_model(clf, folder):
     accuracy = correct/(correct+false)
     print('accuracy: ', accuracy)
 
-def main():
-    choose = int(input("Do you want to train new model[1] or use old one[0]"))
-    if choose == 1:
-        clf = train_model(glob_path_train)
-        joblib.dump(clf, 'train_model.pkl')
-    else:
-        try:
-            clf = joblib.load('train_model.pkl')
-        except FileNotFoundError:
-            clf = train_model(glob_path_train)
+def plot_data(labels, features):
 
-    test_model(clf, glob_path_test)
+    # calculating how many unique clusters there are
+    unique_clusters = []
+    for l in labels:
+        if not l in unique_clusters:
+            unique_clusters.append(l)
+    n_labels = len(unique_clusters)
+
+    # # normalizing clusters' numbers for colors assigning
+    # minimum = min(unique_clusters)
+    # labels_normalized = [x - minimum for x in labels]
+
+    # creating structure for displaying, containing pairs of coordinates and cluster numbers
+    plot_data = []
+    for label, point in zip(labels, features):
+        index = unique_clusters.index(label)
+        plot_data.append([point, index])
+
+    # list of colors for visualization
+    colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c',
+              '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928']
+    if n_labels > len(colors):
+        color = iter(plt.cm.rainbow(np.linspace(0, 1, n_labels)))
+        colors = []
+        for i in range(n_labels):
+            c = next(color)
+            colors.append(c)
+
+    # displaying plot
+    for element in plot_data:
+        point, label = element[0], element[1]
+        plt.scatter(point[0], point[1], color=colors[label], s=5)
+    plt.show()
+
+def visualize_data(folder):
+
+    try:
+        features_mfcc = pickle.load(open("features.py", "rb"))
+    except FileNotFoundError:
+        # Form paths for files from directory path_to_folder
+        paths = paths_from_folder(folder)
+
+        # Load the audio as a waveform `y`
+        # Store the sampling rate as `sr`
+
+        array_y = []
+        array_sr = []
+
+        for path in paths:
+            y, sr = librosa.load(path=path, sr=glob_sr, duration=glob_duration)
+            array_y.append(y)
+            array_sr.append(sr)
+
+        # Extract features using MFCC
+
+        features_mfcc = []
+        for i in range(len(array_y)):
+            current_features = librosa.feature.mfcc(y=array_y[i], sr=array_sr[i], n_mfcc=glob_n_mfcc)[
+                               :glob_n_vectors]  # take first 13 vectors
+            features_mfcc.append(flaten_matrix(current_features))  # add obtained vectors in row
+
+        pickle.dump(features_mfcc, open("features.py", "wb"))
+
+    # Extract label for each file
+
+    labels = []
+    for (dirpath, dirnames, filenames) in walk(folder):
+        labels.extend(extract_genre(filenames, dirpath))
+
+    plot_data(labels, features_mfcc)
+
+def main():
+    # choose = int(input("Do you want to train new model[1] or use old one[0]"))
+    # if choose == 1:
+    #     clf = train_model(glob_path_train)
+    #     joblib.dump(clf, 'train_model.pkl')
+    # else:
+    #     try:
+    #         clf = joblib.load('train_model.pkl')
+    #     except FileNotFoundError:
+    #         clf = train_model(glob_path_train)
+
+    visualize_data(glob_path_train)
+
+    # test_model(clf, glob_path_test)
 
 
 if __name__ == "__main__":
